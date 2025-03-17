39fabbf05762ce906430758a6608598714dac745
Joshua Billings <billingsjoshua07@gmail.com>
2025-03-16 21:03:53 -0500

    Excercise 4

--- app/models.py
+++ app/models.py
@@ -7,8 +7,8 @@ class UserProfile(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     first_name = db.Column(db.String(80))
     last_name = db.Column(db.String(80))
-    username = db.Column(db.String(80), unique=True)
-    password = db.Column(db.String(128))
+    username = db.Column(db.String(255), unique=True)
+    password = db.Column(db.String(512))
 
     def __init__(self, first_name, last_name, username, password):
         self.first_name = first_name
--- app/views.py
+++ app/views.py
@@ -5,11 +5,12 @@ from flask_login import login_user, logout_user, current_user, login_required
 from werkzeug.utils import secure_filename
 from app.models import UserProfile
 from app.forms import LoginForm
+from werkzeug.security import check_password_hash
 
 
-###
+### 
 # Routing for your application.
-###
+### 
 
 @app.route('/')
 def home():
@@ -26,44 +27,53 @@ def about():
 @app.route('/upload', methods=['POST', 'GET'])
 def upload():
     # Instantiate your form class
+    form = UploadForm()  # Ensure you have a form defined for this route
 
     # Validate file upload on submit
     if form.validate_on_submit():
         # Get file data and save to your uploads folder
+        file = form.file.data
+        filename = secure_filename(file.filename)
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
 
         flash('File Saved', 'success')
-        return redirect(url_for('home')) # Update this to redirect the user to a route that displays all uploaded image files
+        return redirect(url_for('home'))  # Redirect to home or another appropriate route
 
-    return render_template('upload.html')
+    return render_template('upload.html', form=form)
 
 
 @app.route('/login', methods=['POST', 'GET'])
 def login():
     form = LoginForm()
 
-    # change this to actually validate the entire form submission
-    # and not just one field
-    if form.username.data:
-        # Get the username and password values from the form.
+    # Check if the form is valid on submit
+    if form.validate_on_submit():
+        # Retrieve the username and password from the form
+        username = form.username.data
+        password = form.password.data
 
-        # Using your model, query database for a user based on the username
-        # and password submitted. Remember you need to compare the password hash.
-        # You will need to import the appropriate function to do so.
-        # Then store the result of that query to a `user` variable so it can be
-        # passed to the login_user() method below.
+        # Query the database to find the user by username
+        user = db.session.execute(db.select(UserProfile).filter_by(username=username)).scalar()
 
-        # Gets user id, load into session
-        login_user(user)
+        # If the user is found and the password matches
+        if user and check_password_hash(user.password, password):
+            # Log the user in
+            login_user(user)
+            flash('Login successful! Redirecting to the upload page...', 'success')
+            return redirect(url_for('upload'))  # Redirect to the /upload route after successful login
 
-        # Remember to flash a message to the user
-        return redirect(url_for("home"))  # The user should be redirected to the upload form instead
-    return render_template("login.html", form=form)
+        # If login fails, flash an error message
+        flash('Invalid username or password. Please try again.', 'danger')
+
+    return render_template('login.html', form=form)
+
 
 # user_loader callback. This callback is used to reload the user object from
 # the user ID stored in the session
 @login_manager.user_loader
 def load_user(id):
     return db.session.execute(db.select(UserProfile).filter_by(id=id)).scalar()
+
 
 ###
 # The functions below should be applicable to all Flask apps.
@@ -77,6 +87,7 @@ def flash_errors(form):
                 getattr(form, field).label.text,
                 error
 ), 'danger')
+
 
 @app.route('/<file_name>.txt')
 def send_text_file(file_name):
